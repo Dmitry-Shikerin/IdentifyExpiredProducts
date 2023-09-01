188abf32e6cefbfc9a8d1f3fb6c6d8910499d320
Dmitry-Shikerin <klubnr4ok@mail.ru>
2023-09-01 15:36:59 +0300

    Добавьте файлы проекта.

---
+++ Определение просрочки/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Определение_просрочки
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Warehouse warehouse = new Warehouse();
+
+            warehouse.Work();
+        }
+    }
+
+    class Conserva
+    {
+        public Conserva(string name, int yearOfProduction, int expirationDate) 
+        {
+            Name = name;
+            YearOfProduction = yearOfProduction;
+            ExpirationDate = expirationDate;
+        }
+
+        public string Name { get; private set; }
+        public int YearOfProduction { get; private set; }
+        public int ExpirationDate { get; private set; }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Название: {Name}, Год изготовления: {YearOfProduction}," +
+                              $" Годен до: {ExpirationDate}");
+        }
+    }
+
+    class Warehouse
+    {
+        private List<Conserva> _conservas = new List<Conserva>();
+
+        private int _currentDate;
+
+        public Warehouse()
+        {
+            _conservas = Create();
+            _currentDate = DateTime.Now.Year;
+        }
+
+        public void Work()
+        {
+            ShowAllConservas();
+            Console.WriteLine();
+            SortConservas();
+        }
+
+        private List<Conserva> Create()
+        {
+            List<Conserva> conservas = new List<Conserva>() 
+            { 
+                new Conserva("Тушеная говядина", 2015, 2019),
+                new Conserva("Перловка", 2019, 2024),
+                new Conserva("Горошек зеленый", 2016, 2020),
+                new Conserva("Кукуруза", 2017, 2023),
+                new Conserva("Маринованные огурцы", 2020, 2024),
+                new Conserva("Кукуруза", 2014, 2018),
+                new Conserva("Тушеная говядина", 2019, 2022),
+                new Conserva("Перловка", 2019, 2024),
+                new Conserva("Горошек зеленый", 2017, 2022),
+            };
+
+            return conservas;
+        }
+
+        private void SortConservas()
+        {
+            var sortredListConservas = _conservas.Where(conserva => conserva.ExpirationDate < _currentDate);
+
+            Console.WriteLine("Просроченные Консервы\n");
+
+            foreach (Conserva conserva in sortredListConservas)
+            {
+                conserva.ShowInfo();
+            }
+        }
+
+        private void ShowAllConservas()
+        {
+            Console.WriteLine("Весь список консерв");
+
+            foreach (Conserva conserva in _conservas)
+            {
+                conserva.ShowInfo();
+            }
+        }
+    }
+}
---
+++ Определение просрочки/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Определение просрочки")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Определение просрочки")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("0d78262e-c54a-4ad9-bc8b-84b60e91a6fa")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
